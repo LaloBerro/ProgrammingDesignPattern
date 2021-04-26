31d78d51f6f64bd59afccf007e50579592969c39
Lalo <laurencioberro@gmail.com>
2021-04-26 17:09:18 -0300

    [A] Monostate

---
+++ Patterns/Assets/Patterns/Patterns Levels/Types/1 Creational/2 Monostate/Scripts/Consumer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Patterns.Creational.Monostate
+{
+	public class Consumer : MonoBehaviour
+	{
+        private void Start()
+        {
+            Debug.Log("The Current Scene is " + new ScenesManagerMonostate().GetCurrentScene());
+        }
+    }
+}
---
+++ Patterns/Assets/Patterns/Patterns Levels/Types/1 Creational/2 Monostate/Scripts/ScenesManagerMonostate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Patterns.Creational.Monostate
+{
+    public class ScenesManagerMonostate
+    {
+        private static string currentScene { get => SceneManager.GetActiveScene().name; }
+
+        public string GetCurrentScene()
+        {
+            return ScenesManagerMonostate.currentScene;
+        }
+    }
+}
